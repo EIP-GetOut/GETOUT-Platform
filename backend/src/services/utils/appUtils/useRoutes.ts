faee78a8a2f090c8d3111eabed1b85e00e9accdc
Alexandre CHETRIT <alexandre.chetrit@epitech.eu>
2022-12-09 10:30:17 +0100

    style: :bulb: typo fault in headers

--- backend/src/services/utils/appUtils/useRoutes.ts
+++ backend/src/services/utils/appUtils/useRoutes.ts
@@ -2,7 +2,7 @@
 ** Copyright GETOUT SAS - All Rights Reserved
 ** Unauthorized copying of this file, via any medium is strictly prohibited
 ** Proprietary and confidential
-** Writed by Alexandre Chetrit <chetrit.pro@hotmail.com>
+** Wrote by Alexandre Chetrit <chetrit.pro@hotmail.com>
 */
 
 import { Application } from 'express'
