ac3725c23423bca87f175f4fed87ffaea5cc4004
Erwan Cariou <erwan1.cariou@epitech.eu>
2022-12-12 18:22:12 +0100

    feat(app): :lipstick: add a real home page (like the figma one)

--- app/lib/layouts/home.dart
+++ app/lib/layouts/home.dart
@@ -18,24 +18,58 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final double phoneHeight = MediaQuery.of(context).size.height;
+    final double phoneWidth = MediaQuery.of(context).size.width;
+    bool isLandscape = (phoneWidth > phoneHeight);
     return Scaffold(
-        floatingActionButton: Center(
-        child: SizedBox(
-          width: 300,
-          height: 100,
-          child: FloatingActionButton(
-            shape: const RoundedRectangleBorder(),
-              onPressed: () =>
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => MainPlaygroundPage())),
-              child: const Text('Go to playground',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ))),
-              )
-          )
+      body: Column(children: [
+        Row(children: [
+                SizedBox(width: (isLandscape ? 20 : 10) * phoneWidth / 100),
+                SizedBox(
+                  height: (isLandscape ? 20 : 27) * phoneHeight / 100,
+                  width: (isLandscape ? 20 : 27) * phoneWidth / 100,
+                  child: Image.asset("assets/GetOut_logo.png")),
+                SizedBox(width: (isLandscape ? 4 : 8) * phoneWidth / 100),
+                const Text("GETOUT",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ))
+        ]),
+        SizedBox(
+          height: (isLandscape ? 49 : 30) * phoneHeight / 100,
+          width: (isLandscape ? 47 : 80) * phoneWidth / 100,
+          child: Align(
+          alignment: Alignment.topCenter,
+            child: Image.asset("assets/jesaispas.png")),
+        ),
+        SizedBox(
+            height: 13 * phoneHeight / 100,
+            width: 95 * phoneWidth / 100,
+            child: const Text("Devenez plus productif en réduisant le temps passé sur les réseaux sociaux",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+            )))
+        ]),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: SizedBox(
+              width: 85 * phoneWidth / 100,
+              height: 65,
+              child: FloatingActionButton(
+                  shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(50.0))),
+                  backgroundColor: const Color(0xFF584CF4),
+                  onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const MainPlaygroundPage())),
+                  child: const Text('Commencer l\'aventure',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 19,
+                    ))
+            )
+        )
     );
   }
 }
