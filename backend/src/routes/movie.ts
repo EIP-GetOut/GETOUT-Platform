5ee0e49f401e5c6b2fda74e5d23ebcd92dc737b7
Enzo VALLET <enzo1.vallet@epitech.eu>
2022-12-13 15:27:46 +0100

    refactor(backend): :recycle: getDetails only return useful informations

--- backend/src/routes/movie.ts
+++ backend/src/routes/movie.ts
@@ -1,17 +1,18 @@
 /*
-** Copyright Area - All Rights Reserved
+** Copyright GETOUT SAS - All Rights Reserved
 ** Unauthorized copying of this file, via any medium is strictly prohibited
 ** Proprietary and confidential
-** Wrote by Théo de Boysson <theo.de-boysson@epitech.eu>
+** Wrote by Firstname Lastname <firstname.lastname@domain.com>
 */
+
+import { Request, Response, Router } from "express";
+import { query } from "express-validator";
+import { getReasonPhrase, StatusCodes } from "http-status-codes";
+import { MovieResponse }  from 'moviedb-promise'
 
 import logger, { logApiRequest } from "@services/middlewares/logging";
 import validate from "@services/middlewares/validator";
-import { query } from "express-validator";
-import { Request, Response, Router } from "express";
 
-import { MovieResponse }  from 'moviedb-promise'
-import { getReasonPhrase, StatusCodes } from "http-status-codes";
 import { getDetail } from "@models/movie";
 
 const router = Router();
@@ -26,8 +27,17 @@ router.get('/movie/:id', rulesGet, validate, logApiRequest, (req: Request, res: 
         if (!movieObtained) {
             return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR))
         }
+        const movie = {
+            title: movieObtained.title,
+            overview: movieObtained.overview,
+            poster_path: movieObtained.poster_path,
+            backdrop_path: movieObtained.backdrop_path,
+            release_date: movieObtained.release_date,
+            vote_average: Number(movieObtained.vote_average) / 2,
+            duration: Number(movieObtained.runtime) / 60 - (Number(movieObtained.runtime) / 60 % 1) + 'h' + Number(movieObtained.runtime) % 60 + 'min',
+        }
         return res.status(StatusCodes.OK).json({
-            movieObtained
+            movie
         })
     })
 })
