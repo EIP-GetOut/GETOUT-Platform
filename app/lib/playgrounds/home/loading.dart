54849f920b6ef664de2e32e571bd54caf17ae78c
Erwan Cariou <erwan1.cariou@epitech.eu>
2022-12-15 14:59:50 +0100

    feat(app): :lipstick: beautiful loading page

--- app/lib/playgrounds/home/loading.dart
+++ app/lib/playgrounds/home/loading.dart
@@ -13,11 +13,11 @@
 */
 
 import "package:flutter/material.dart";
-import 'package:getout/constants/movie_genre.dart';
 import 'package:getout/playgrounds/home/dashboard.dart';
 
 import 'package:getout/models/requests/generate_movies.dart';
 import 'package:getout/services/requests/requests_service.dart';
+import 'package:getout/models/flex_size.dart';
 
 import 'dart:math';
 
@@ -26,7 +26,7 @@ void toto() {
 }
 class LoadingPage extends StatefulWidget {
   LoadingPage({Key? key}) : super(key: key);
-  List<int> genre = [28,12,16,35,80,99,18,10751,14,36,27,10402,9648,10749,878,10770,53,10752,37];
+  List<int> genre = [28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 10402, 9648, 10749, 878, 10770, 53, 10752, 37];
 
   @override
   State<LoadingPage> createState() => _LoadingPageState();
@@ -36,6 +36,8 @@ class LoadingPage extends StatefulWidget {
 class _LoadingPageState extends State<LoadingPage> {
   @override
   Widget build(BuildContext context) {
+    bool isLandscape = (MediaQuery.of(context).size.width > MediaQuery.of(context).size.height);
+
     WidgetsBinding.instance.addPostFrameCallback((_) {
       GenerateMoviesRequest request = GenerateMoviesRequest(genres: [widget.genre[Random().nextInt(19)], widget.genre[Random().nextInt(19)]]);
       RequestsService.instance.generateMovies(request).then((GenerateMoviesResponse moviesResponse) {
@@ -48,17 +50,33 @@ class _LoadingPageState extends State<LoadingPage> {
       });
     });
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.white,
       body: NestedScrollView(
         floatHeaderSlivers: true,
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
           return <Widget>[];
         },
-        body: CircularProgressIndicator(
+        body: Column(children: [
+          SizedBox(height: perHeight(context, (isLandscape ? 25 : 30))),
+          Row(children: [
+            SizedBox(width: perWidth(context, (isLandscape ? 25 : 10))),
+            SizedBox(
+              height: uniHeight(context, 13, isLandscape),
+              width: uniWidth(context, 27, isLandscape),
+              child: Image.asset('assets/GetOut_logo.png')),
+            SizedBox(width: perWidth(context, 4)),
+            SizedBox(
+                height: uniHeight(context, 10, isLandscape),
+                width: uniWidth(context, 45, isLandscape),
+                child: Image.asset('assets/GetOut_text.png'))]),
+          SizedBox(height: perHeight(context, (isLandscape ? 8 : 8))),
+          const SizedBox(
+          height: 85,
+          width: 85,
+          child: CircularProgressIndicator(
           backgroundColor: Colors.transparent,
-          color: Colors.blue,
         ),
-      ),
-    );
+      )]),
+    ));
   }
 }
