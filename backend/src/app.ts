6709d4bb9595b778b241f79f55c42144cd041e64
Julien Letoux <julien.letoux@epitech.eu>
2023-04-27 11:47:35 +0200

    feat(backend): :sparkles: introducing the database
    database with basic account table

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,14 +11,21 @@ import logger from '@middlewares/logging'
 
 import { useRoutes, useMiddlewares } from '@services/utils/appUtils/appUtils'
 
+import { AppDataSource } from '@config/dataSource'
+
 (() => {
   const app: Application = express()
   const port: string | undefined = process.env.PORT
 
-  useMiddlewares(app)
-  useRoutes(app)
+  AppDataSource.initialize().then(() => {
+    logger.info("Data Source has been initialized!")
+    useMiddlewares(app)
+    useRoutes(app)
 
-  app.listen(port, () => {
+    app.listen(port, () => {
       logger.info(`App listening at http://localhost:${port}`)
     })
+  }).catch((err) => {
+    logger.error("Error during Data Source initialization:", err)
+  })
 })()
