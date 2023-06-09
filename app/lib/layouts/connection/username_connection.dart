596279488aadca4ce440e59e17061852e28c2710
Perry Chouteau <perrychouteau@Perrys-MacBook-Pro.local>
2023-06-09 22:24:59 +0200

    feat(dev-oauth): :sparkles: add (dev)google-oauth2

--- app/lib/layouts/connection/username_connection.dart
+++ app/lib/layouts/connection/username_connection.dart
@@ -4,6 +4,7 @@ import 'package:GetOut/layouts/connection/register.dart';
 import 'package:GetOut/models/sign/fields.dart';
 import 'package:GetOut/services/requests/requests_service.dart';
 import 'package:GetOut/layouts/welcome.dart';
+import 'package:GetOut/services/google/google_signin_api.dart';
 
 class ConnectionPage extends StatefulWidget {
   const ConnectionPage({Key? key}) : super(key: key);
@@ -21,26 +22,30 @@ class _ConnectionPageState extends State<ConnectionPage> {
   bool isLoading = false;
 
   Future<VoidCallback?> loginPressed() async {
-    if (_emailKey.currentState!.validate() && _passwordKey.currentState!.validate()) {
-          setState(() {
-      isLoading = true;
-    });
-    print("avant");
-    LoginResponseInfo res = await RequestsService.instance.login(
-        LoginRequest(email: emailController.text,
-            password: passwordController.text));
-    print("res = ");
-    print(res.email);
-    if (res.statusCode == LoginResponseInfo.success) {
-      return Navigator.push(context, MaterialPageRoute(builder: (context) => const WelcomePage()));
-    }
-    setState(() {
-      isLoading = false;
-    });
-    /// TODO : show error
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text((res.statusCode == 502) ? ('No internet connection') : ('ERROR'))));
-    return null;
+    if (_emailKey.currentState!.validate() &&
+        _passwordKey.currentState!.validate()) {
+      setState(() {
+        isLoading = true;
+      });
+      print("avant");
+      LoginResponseInfo res = await RequestsService.instance.login(LoginRequest(
+          email: emailController.text, password: passwordController.text));
+      print("res = ");
+      print(res.email);
+      if (res.statusCode == LoginResponseInfo.success) {
+        return Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const WelcomePage()));
+      }
+      setState(() {
+        isLoading = false;
+      });
+
+      /// TODO : show error
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text((res.statusCode == 502)
+              ? ('No internet connection')
+              : ('ERROR'))));
+      return null;
     }
   }
 
@@ -69,12 +74,15 @@ class _ConnectionPageState extends State<ConnectionPage> {
                         Padding(
                           padding: const EdgeInsets.symmetric(
                               horizontal: 8, vertical: 16),
-                          child: MailField(formKey: _emailKey, controller: emailController),
+                          child: MailField(
+                              formKey: _emailKey, controller: emailController),
                         ),
                         Padding(
                           padding: const EdgeInsets.symmetric(
                               horizontal: 8, vertical: 16),
-                          child: PasswordField(formKey: _passwordKey, controller: passwordController),
+                          child: PasswordField(
+                              formKey: _passwordKey,
+                              controller: passwordController),
                         ),
                         SizedBox(
                           child: Align(
@@ -85,60 +93,45 @@ class _ConnectionPageState extends State<ConnectionPage> {
                               )),
                         ),
                         const SizedBox(height: 20),
-                        Row(
-                          mainAxisAlignment:
-                              MainAxisAlignment.spaceEvenly, // <-- SEE HERE
-                          children: [
-                            SizedBox(
-                              child: Align(
-                                  alignment: Alignment.center,
-                                  child: Image.asset(
-                                    'assets/Twitter.png',
-                                    fit: BoxFit.contain,
-                                  )),
-                            ),
-                            SizedBox(
-                              child: Align(
-                                  alignment: Alignment.center,
-                                  child: Image.asset(
-                                    'assets/Google.png',
-                                    fit: BoxFit.contain,
-                                  )),
-                            ),
-                            SizedBox(
-                              child: Align(
-                                  alignment: Alignment.center,
-                                  child: Image.asset(
-                                    'assets/Facebook.png',
-                                    fit: BoxFit.contain,
-                                  )),
-                            ),
-                          ],
+                        ElevatedButton.icon (
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            foregroundColor: Colors.black,
+                            minimumSize: const Size(double.infinity, 50),
+                          ),
+                          icon: Image.asset('assets/Google.png', width: 40,),
+                          // Falcon label: Text('Sign Up with Google'),
+                          label: const Text('Se connecter ave Google'),
+                          onPressed: signIn,
                         ),
-                        // Row(
                         const SizedBox(
                           height: 30,
                         ),
                         GestureDetector(
                           onTap: () {
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => const RegisterPage()));
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const RegisterPage()));
                           },
                           child: const Text.rich(
-                          TextSpan(
-                            text: 'Première connection ?',
-                            style: TextStyle(color: Colors.black, fontSize: 16),
-                            children: <InlineSpan>[
-                              
-                              TextSpan(
-                                text: ' Créer un compte',
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: Color.fromRGBO(213, 86, 65, 0.992)),
-                              ),
-                            ],
+                            TextSpan(
+                              text: 'Première connection ?',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 16),
+                              children: <InlineSpan>[
+                                TextSpan(
+                                  text: ' Créer un compte',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color:
+                                          Color.fromRGBO(213, 86, 65, 0.992)),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
                         ),
                       ]),
                 ),
@@ -163,5 +156,19 @@ class _ConnectionPageState extends State<ConnectionPage> {
                   color: Colors.white,
                   fontSize: 19,
                 ))));
+  }
+
+  Future signIn() async {
+    final user = await GoogleSignInApi.login();
+    if (user != null &&
+        user.email != null &&
+        user.displayName != null &&
+        user.photoUrl != null) {
+      debugPrint(
+          "${user.email} ${user.displayName ?? ""} ${user.photoUrl ?? ""}");
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+              "Info ${user.email} ${user.displayName ?? ""} ${user.photoUrl ?? ""}")));
+    }
   }
 }
