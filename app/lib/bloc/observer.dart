a657af716309264f6ba97f1f43ba15a52bfc6ea0
Chouteau Perry <perry.chouteau@epitech.eu>
2023-07-21 02:15:37 +0200

    feat(devs/setup_intl_bloc): :sparkles: setup intl(l10n) and bloc. handle material_app.locale using bloc and l10n

---
+++ app/lib/bloc/observer.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+
+/// {@template app_bloc_observer}
+/// Custom [BlocObserver] that observes all bloc and cubit state changes.
+/// {@endtemplate}
+class AppBlocObserver extends BlocObserver {
+  /// {@macro app_bloc_observer}
+  const AppBlocObserver();
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    if (bloc is Cubit) {
+      print(change);
+    }
+  }
+
+  @override
+  void onTransition(
+      Bloc<dynamic, dynamic> bloc, Transition<dynamic, dynamic> transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+}
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,14 +2,21 @@
 ** Copyright GETOUT SAS - All Rights Reserved
 ** Unauthorized copying of this file, via any medium is strictly prohibited
 ** Proprietary and confidential
-** Wrote by Erwan Cariou <erwan1.cariou@epitech.eu>
+** Wrote by Erwan Cariou <erwan1.cariou@epitech.eu>, Perry Chouteau <perry.chouteau@epitech.eu>
 */
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:getout/bloc/locale_bloc.dart';
+import 'package:getout/bloc/observer.dart';
+import 'package:getout/bloc/locale_bloc.dart';
+import 'package:getout/layouts/connection/login.dart';
 import 'package:getout/models/category.dart';
 import 'package:getout/layouts/home/loading_session.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:getout/theme.dart';
+
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 Map<int, Color> colorMap = {
   50: const Color.fromRGBO(213, 86, 65, .1),
@@ -24,9 +31,26 @@ Map<int, Color> colorMap = {
   900: const Color.fromRGBO(213, 86, 65, 1),
 };
 
-void main() => runApp(MyApp());
+void main() {
+  Bloc.observer = const AppBlocObserver(); // BLoC MidleWare.
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => LocaleBloc()),
+      ],
+      child: MyAppView(),
+    );
+  }
+}
+
+class MyAppView extends StatelessWidget {
   final List<Category> categories = [
     Category(title: 'Horreur'),
     Category(title: 'Comedie'),
@@ -34,15 +58,27 @@ class MyApp extends StatelessWidget {
     Category(title: 'Science-Fiction'),
   ];
 
-  MyApp({super.key});
+  MyAppView({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Get Out',
-        localizationsDelegates: const [GlobalMaterialLocalizations.delegate],
-        supportedLocales: const [Locale('en'), Locale('fr')],
-        theme: getOutTheme,
-        // home: const WelcomePage(),
-        home: const LoadingPageSession());
+    return Builder(builder: (context) {
+      final locale = context.watch<LocaleBloc>().state;
+
+      return MaterialApp(
+          title: 'Get Out',
+          locale: locale,
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate
+          ],
+          supportedLocales: AppLocalizations.supportedLocales,
+          theme: getOutTheme,
+          // home: const WelcomePage(),
+//          home: const LoadingPageSession());
+          home: const ConnectionPage());
+      // return a Widget which depends on the state of BlocA, BlocB, and BlocC
+    });
   }
 }
