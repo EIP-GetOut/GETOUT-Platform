7f6bb34c4c7ee7b1dfabcdb11552c54cfbdfbe55
Inès <ines.maaroufi@epitech.eu>
2024-01-09 19:02:05 -0500

    feat(app): :sparkles: add a switch button to the notifications button and add the correct styles

--- app/lib/services/notifications/notifications_page.dart
+++ app/lib/services/notifications/notifications_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'notifications.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 
 
 class NotificationsPage extends StatefulWidget  {
@@ -12,33 +13,62 @@ class NotificationsPage extends StatefulWidget  {
 class _NotificationsPageState extends State<NotificationsPage> {
 
   NotificationsServices notificationsServices = NotificationsServices();
+  bool status = false;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          title: const Text('Notifications'),
+          title: const Text('NOTIFICATIONS'),
           leading: const BackButton(),
         ),
-        body: Center(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
           child: Column(
             children: [
-          ElevatedButton(
-            onPressed: () {
-              notificationsServices.sendNotif();
-            },
-            child: Text('Show Notification'),
-          ),
-                    ElevatedButton(
-            onPressed: () {
-              notificationsServices.stopNotif();
-            },
-            child: Text('Stop Notification'),
-          )
-        ]),
-        )
-      ),
-    );
+            const SizedBox(height: 50),
+              Container(
+              color : Colors.grey[500],
+              width: 500,
+              height: 30,
+              child: const Text('Notifications Push', style: TextStyle(fontSize: 20, color: Colors.white, )),
+            ),
+            const SizedBox(height: 30),
+            Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+            Text('Recommandations', style: TextStyle(fontSize: 20, color: Colors.grey[800])),
+            FlutterSwitch(
+              height: 35.0,
+              width: 65.0,
+              padding: 6.0,
+              toggleSize: 20.0,
+              borderRadius: 20.0,
+                value: status,
+                activeColor: const Color.fromRGBO(213, 86, 65, 1),
+                onToggle: (val) {
+                  setState(() {
+                    status = val;
+                  });
+                },
+            ),
+        ])])));
+        // Center(
+        //   child: Column(
+        //     children: [
+        //   ElevatedButton(
+        //     onPressed: () {
+        //       notificationsServices.sendNotif();
+        //     },
+        //     child: Text('Show Notification'),
+        //   ),
+        //             ElevatedButton(
+        //     onPressed: () {
+        //       notificationsServices.stopNotif();
+        //     },
+        //     child: Text('Stop Notification'),
+        //   )
+        // ]),
+        // )
   }
 }
