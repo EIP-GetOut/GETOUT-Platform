172e501a889bbec53c5d509a2e6ddaa126abab08
zachmae <zacharie.lawson@epitech.eu>
2024-01-29 22:59:05 +0900

    feat(recommandations): :sparkles: books score are now properly evaluated

--- backend/src/services/recommandations/books.py
+++ backend/src/services/recommandations/books.py
@@ -65,20 +65,41 @@ def calculate_score(book, user):
     score += calculate_popularity_score(book, user)
     print("total score: " + str(score))
     print("---------------------------------")
-    score = random.randint(1, 100)
     return score
 
 def calculate_genre_score(book, user):
     score = 0
-
+    google_books_api = build("books", "v1", developerKey=os.getenv("GOOGLE_BOOKS_KEY"))
+    response = google_books_api.volumes().list(q="isbn:" + book["primary_isbn13"]).execute()
+    if "items" in response:
+        for g in response["items"][0]["volumeInfo"]["categories"]:
+            if g in user["preferences"]["books"]:
+                score += (WEIGHTS["genres"] * 100) / len(response["items"][0]["volumeInfo"]["categories"])
+    print("genre score: " + str(score))
     return score
 
 def calculate_critics_score(book, user):
     score = 0
+    google_books_api = build("books", "v1", developerKey=os.getenv("GOOGLE_BOOKS_KEY"))
+    response = google_books_api.volumes().list(q="isbn:" + book["primary_isbn13"]).execute()
+    if "items" in response:
+        try:
+            score = (response["items"][0]["volumeInfo"]["averageRating"] * 20) * WEIGHTS["critics"]
+        except:
+            print("no rating")
+    print("critics score: " + str(score))
     return score
 
 def calculate_popularity_score(book, user):
     score = 0
+    google_books_api = build("books", "v1", developerKey=os.getenv("GOOGLE_BOOKS_KEY"))
+    response = google_books_api.volumes().list(q="isbn:" + book["primary_isbn13"]).execute()
+    if "items" in response:
+        try:
+            score = (response["items"][0]["volumeInfo"]["ratingsCount"] / 100000) * WEIGHTS["popularity"]
+        except:
+            print("no rating")
+    print("popularity score: " + str(score))
     return score
 
     #api = GoogleBooksAPI()
