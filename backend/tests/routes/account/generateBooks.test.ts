3d9cb8127f2198a1abb490598e26301e654f74e7
Alexandre CHETRIT <chetrit.pro@hotmail.com>
2024-01-29 21:00:26 +0700

    fix: :bug: reordering the unit tests for restored generate movies / books routes

--- backend/tests/routes/account/generateBooks.test.ts
+++ backend/tests/routes/account/generateBooks.test.ts
@@ -7,7 +7,6 @@
 
 import { beforeAll, expect, it } from '@jest/globals'
 import { StatusCodes } from 'http-status-codes'
-import { type UUID } from 'node:crypto'
 import { describe } from 'node:test'
 import request from 'supertest'
 
@@ -21,7 +20,6 @@ const loginBody = {
 }
 
 describe('Books Routes', () => {
-  let accountId: UUID
   let cookie: string
 
   beforeAll(async () => {
@@ -30,20 +28,18 @@ describe('Books Routes', () => {
       if (optionalCookie === null) { throw Error('Failed extracting cookie') }
       cookie = optionalCookie
       return await request(app).get('/session').set('Cookie', cookie)
-    }).then((res) => {
-      accountId = res.body.account.id
     })
   })
 
-  it('should respond with 200 OK for GET /recommend-books?intitle=fleurs', async () => {
-    await request(app).get(`/account/${accountId}/recommend-books?intitle=fleurs`).set('Cookie', cookie).then((response) => {
+  it('should respond with 200 OK for GET /generate-books?intitle=fleurs', async () => {
+    await request(app).get('/generate-books?intitle=fleurs').set('Cookie', cookie).then((response) => {
       expect(response.status).toBe(StatusCodes.OK)
       expect(response.body.books).toHaveLength(5)
     })
   })
 
   it('should respond with 500 Bad Request', async () => {
-    await request(app).get(`/account/${accountId}/recommend-books`).set('Cookie', cookie).then((response) => {
+    await request(app).get('/generate-books').set('Cookie', cookie).then((response) => {
       expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
       // eslint-disable-next-line no-useless-escape
       expect(response.text).toBe('{\"name\":\"ApiError\",\"message\":\"Bad Request\"}')
--- backend/tests/routes/account/generateMovies.test.ts
+++ backend/tests/routes/account/generateMovies.test.ts
@@ -6,7 +6,6 @@
 */
 
 import { beforeAll, expect, it } from '@jest/globals'
-import { type UUID } from 'node:crypto'
 import { describe } from 'node:test'
 import request from 'supertest'
 
@@ -20,7 +19,6 @@ const loginBody = {
 }
 
 describe('Movies Routes', () => {
-  let accountId: UUID
   let cookie: string
 
   beforeAll(async () => {
@@ -29,13 +27,11 @@ describe('Movies Routes', () => {
       if (optionalCookie === null) { throw Error('Failed extracting cookie') }
       cookie = optionalCookie
       return await request(app).get('/session').set('Cookie', cookie)
-    }).then((res) => {
-      accountId = res.body.account.id
     })
   })
 
-  it('should respond with 200 OK for GET /recommend-movies?with_genres=27', async () => {
-    await request(app).get(`/account/${accountId}/recommend-movies?with_genres=27`).set('Cookie', cookie).then((response) => {
+  it('should respond with 200 OK for GET /generate-movies?with_genres=27', async () => {
+    await request(app).get('/generate-movies?with_genres=27').set('Cookie', cookie).then((response) => {
       expect(response.status).toBe(200)
       expect(response.body.movies).toHaveLength(5)
     })
