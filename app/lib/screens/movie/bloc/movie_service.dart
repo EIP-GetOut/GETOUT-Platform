6408ab78c077e58335652c5cee83b84010210e4b
Inès MAAROUFI <57877506+Happinesseuh@users.noreply.github.com>
2024-03-20 14:33:07 -0400

    feat(app): :sparkles: add like and dislike feature

--- app/lib/screens/movie/bloc/movie_service.dart
+++ app/lib/screens/movie/bloc/movie_service.dart
@@ -14,6 +14,8 @@ import 'package:getout/constants/http_status.dart';
 import 'package:getout/global.dart' as globals;
 
 class MovieService {
+  final String userId = globals.session?['id'].toString() ?? '';
+
   Future<InfoMovieResponse> getInfoMovie(CreateInfoMovieRequest request) async {
     InfoMovieResponse result =
         const InfoMovieResponse(statusCode: HttpStatus.APP_ERROR);
@@ -46,18 +48,23 @@ class MovieService {
       }
 
       result = InfoMovieResponse(
-        title: data['movie']['title'],
-        overview: data['movie']['overview'] ?? 'Pas de description disponible',
-        posterPath: data['movie']['poster_path'],
-        backdropPath: data['movie']['backdrop_path'],
-        releaseDate: data['movie']['release_date'],
-        voteAverage: data['movie']['vote_average'],
-        duration: (data['movie']['duration'] == '0h0min')
-            ? 'N/A'
-            : data['movie']['duration'],
-        cast: parseCast(data['movie']['cast']),
-        statusCode: response?.statusCode ?? 500,
-      );
+          title: data['movie']['title'],
+          overview:
+              data['movie']['overview'] ?? 'Pas de description disponible',
+          posterPath: data['movie']['poster_path'],
+          backdropPath: data['movie']['backdrop_path'],
+          releaseDate: data['movie']['release_date'],
+          voteAverage: data['movie']['vote_average'],
+          duration: (data['movie']['duration'].toString() == '0')
+              ? 'N/A'
+              : data['movie']['duration'],
+          cast: parseCast(data['movie']['cast']),
+          statusCode: response?.statusCode ?? 500,
+          liked: globals.session?['likedMovies'].contains(request.id),
+          disliked: globals.session?['dislikedMovies'].contains(request.id),
+          wishlisted: globals.session?['watchlist'].contains(request.id),
+          seen: globals.session?['seenMovies'].contains(request.id),
+          id: request.id);
     } catch (error) {
       if (error.toString() == 'Connection reset by peer' ||
           error.toString() ==
@@ -67,5 +74,230 @@ class MovieService {
       return result;
     }
     return result;
+  }
+
+  Future<AddMovieResponse> addLikedMovie(AddMovieRequest request) async {
+    AddMovieResponse result =
+        const AddMovieResponse(statusCode: HttpStatus.APP_ERROR);
+
+    try {
+      final response = await globals.dio?.post(
+          '${ApiConstants.rootApiPath}${ApiConstants.accountPath}/$userId${ApiConstants.addLikedMoviePath}',
+          options: Options(
+            headers: {'Content-Type': 'application/json'},
+          ),
+          data: {'movieId': request.id});
+      if (response?.statusCode != HttpStatus.CREATED) {
+        return AddMovieResponse(statusCode: response?.statusCode ?? 500);
+      }
+
+      result = AddMovieResponse(
+        statusCode: response?.statusCode ?? 500,
+      );
+      await globals.sessionManager.getSession();
+      return result;
+    } on DioException {
+      // add "catch (dioError)" for debugging
+      rethrow;
+    } catch (dioError) {
+      rethrow;
+    }
+  }
+
+  Future<AddMovieResponse> removeLikedMovie(AddMovieRequest request) async {
+    AddMovieResponse result =
+        const AddMovieResponse(statusCode: HttpStatus.APP_ERROR);
+
+    try {
+      final response = await globals.dio?.delete(
+        '${ApiConstants.rootApiPath}${ApiConstants.accountPath}/$userId${ApiConstants.addLikedMoviePath}/${request.id}',
+        options: Options(
+          headers: {'Content-Type': 'application/json'},
+        ),
+      );
+      if (response?.statusCode != HttpStatus.OK) {
+        return AddMovieResponse(statusCode: response?.statusCode ?? 500);
+      }
+
+      result = AddMovieResponse(
+        statusCode: response?.statusCode ?? 500,
+      );
+      await globals.sessionManager.getSession();
+      return result;
+    } on DioException {
+      // add "catch (dioError)" for debugging
+      rethrow;
+    } catch (dioError) {
+      rethrow;
+    }
+  }
+
+  Future<AddMovieResponse> removeDislikedMovie(AddMovieRequest request) async {
+    AddMovieResponse result =
+        const AddMovieResponse(statusCode: HttpStatus.APP_ERROR);
+
+    try {
+      final response = await globals.dio?.delete(
+        '${ApiConstants.rootApiPath}${ApiConstants.accountPath}/$userId${ApiConstants.addDislikedMoviePath}/${request.id}',
+        options: Options(
+          headers: {'Content-Type': 'application/json'},
+        ),
+      );
+      if (response?.statusCode != HttpStatus.OK) {
+        return AddMovieResponse(statusCode: response?.statusCode ?? 500);
+      }
+
+      result = AddMovieResponse(
+        statusCode: response?.statusCode ?? 500,
+      );
+
+      await globals.sessionManager.getSession();
+      return result;
+    } on DioException {
+      // add "catch (dioError)" for debugging
+      rethrow;
+    } catch (dioError) {
+      rethrow;
+    }
+  }
+
+  Future<AddMovieResponse> addDislikedMovie(AddMovieRequest request) async {
+    AddMovieResponse result =
+        const AddMovieResponse(statusCode: HttpStatus.APP_ERROR);
+
+    try {
+      final response = await globals.dio?.post(
+          '${ApiConstants.rootApiPath}${ApiConstants.accountPath}/$userId${ApiConstants.addDislikedMoviePath}',
+          options: Options(
+            headers: {'Content-Type': 'application/json'},
+          ),
+          data: {'movieId': request.id});
+
+      if (response?.statusCode != HttpStatus.CREATED) {
+        return AddMovieResponse(statusCode: response?.statusCode ?? 500);
+      }
+
+      result = AddMovieResponse(
+        statusCode: response?.statusCode ?? 500,
+      );
+
+      await globals.sessionManager.getSession();
+      return result;
+    } on DioException {
+      rethrow;
+    } catch (dioError) {
+      rethrow;
+    }
+  }
+
+  Future<AddMovieResponse> addWishlistedMovie(AddMovieRequest request) async {
+    AddMovieResponse result =
+        const AddMovieResponse(statusCode: HttpStatus.APP_ERROR);
+
+    try {
+      final response = await globals.dio?.post(
+          '${ApiConstants.rootApiPath}${ApiConstants.accountPath}/$userId${ApiConstants.watchlistPath}',
+          options: Options(
+            headers: {'Content-Type': 'application/json'},
+          ),
+          data: {'movieId': request.id});
+      if (response?.statusCode != HttpStatus.CREATED) {
+        return AddMovieResponse(statusCode: response?.statusCode ?? 500);
+      }
+
+      result = AddMovieResponse(
+        statusCode: response?.statusCode ?? 500,
+      );
+      await globals.sessionManager.getSession();
+      return result;
+    } on DioException {
+      // add "catch (dioError)" for debugging
+      rethrow;
+    } catch (dioError) {
+      rethrow;
+    }
+  }
+
+  Future<AddMovieResponse> removeWishlistedMovie(
+      AddMovieRequest request) async {
+    AddMovieResponse result =
+        const AddMovieResponse(statusCode: HttpStatus.APP_ERROR);
+
+    try {
+      final response = await globals.dio?.delete(
+          '${ApiConstants.rootApiPath}${ApiConstants.accountPath}/$userId${ApiConstants.watchlistPath}/${request.id}',
+          options: Options(
+            headers: {'Content-Type': 'application/json'},
+          ));
+      if (response?.statusCode != AddMovieResponse.success) {
+        return AddMovieResponse(statusCode: response?.statusCode ?? 500);
+      }
+
+      result = AddMovieResponse(
+        statusCode: response?.statusCode ?? 500,
+      );
+      await globals.sessionManager.getSession();
+      return result;
+    } on DioException {
+      // add "catch (dioError)" for debugging
+      rethrow;
+    } catch (dioError) {
+      rethrow;
+    }
+  }
+
+  Future<AddMovieResponse> addSeenMovie(AddMovieRequest request) async {
+    AddMovieResponse result =
+        const AddMovieResponse(statusCode: HttpStatus.APP_ERROR);
+
+    try {
+      final response = await globals.dio?.post(
+          '${ApiConstants.rootApiPath}${ApiConstants.accountPath}/$userId${ApiConstants.seenMoviesPath}',
+          options: Options(
+            headers: {'Content-Type': 'application/json'},
+          ),
+          data: {'movieId': request.id});
+      if (response?.statusCode != HttpStatus.CREATED) {
+        return AddMovieResponse(statusCode: response?.statusCode ?? 500);
+      }
+
+      result = AddMovieResponse(
+        statusCode: response?.statusCode ?? 500,
+      );
+      await globals.sessionManager.getSession();
+      return result;
+    } on DioException {
+      // add "catch (dioError)" for debugging
+      rethrow;
+    } catch (dioError) {
+      rethrow;
+    }
+  }
+
+  Future<AddMovieResponse> removeSeenMovie(AddMovieRequest request) async {
+    AddMovieResponse result =
+        const AddMovieResponse(statusCode: HttpStatus.APP_ERROR);
+
+    try {
+      final response = await globals.dio?.delete(
+          '${ApiConstants.rootApiPath}${ApiConstants.accountPath}/$userId${ApiConstants.seenMoviesPath}/${request.id}',
+          options: Options(
+            headers: {'Content-Type': 'application/json'},
+          ));
+      if (response?.statusCode != AddMovieResponse.success) {
+        return AddMovieResponse(statusCode: response?.statusCode ?? 500);
+      }
+
+      result = AddMovieResponse(
+        statusCode: response?.statusCode ?? 500,
+      );
+      await globals.sessionManager.getSession();
+      return result;
+    } on DioException {
+      // add "catch (dioError)" for debugging
+      rethrow;
+    } catch (dioError) {
+      rethrow;
+    }
   }
 }
