501551b9fe981b126c402b7647234f32ff160aa7
Alexandre CHETRIT <chetrit.pro@hotmail.com>
2024-04-18 19:47:31 +0700

    refactor(backend): :heavy_minus_sign: removed node-fetch and github oauth

--- backend/src/services/authentification.ts
+++ backend/src/services/authentification.ts
@@ -6,12 +6,8 @@
 */
 
 import { OAuth2Client } from 'google-auth-library'
-import { type Response } from 'node-fetch'
 
 import { type oauthAccount } from '@routes/account/oauth/oauthAccount'
-
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports
-const fetch = async (...args: Parameters<typeof import('node-fetch')['default']>): Promise<Response> => await import('node-fetch').then(async ({ default: fetch }) => await fetch(...args))
 
 async function authentifyWithGoogle (account: oauthAccount): Promise<[boolean, any]> {
   const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
@@ -29,41 +25,4 @@ async function authentifyWithGoogle (account: oauthAccount): Promise<[boolean, a
   })
 }
 
-async function getAccountInfoWithGithubCode (code: string): Promise< any | null> {
-  if (code == null) { return await Promise.resolve(null) }
-
-  const authorizeUrlGithub = new URL('https://github.com/login/oauth/access_token')
-  authorizeUrlGithub.search = new URLSearchParams({
-    client_id: process.env.GITHUB_CLIENT_ID ?? '',
-    client_secret: process.env.GITHUB_CLIENT_SECRET ?? '',
-    code
-  }).toString()
-
-  return await fetch(authorizeUrlGithub.toString(), {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json'
-    }
-  }).then(async (res) => {
-    if (!res.ok) {
-      throw Error(res.statusText)
-    }
-    return await res.json()
-  }).then(async (data: any) => {
-    return await fetch('https://api.github.com/user', {
-      headers: { Authorization: `${data.token_type} ${data.access_token}` }
-    })
-  }).then(async (res) => {
-    if (!res.ok) {
-      throw Error(res.statusText)
-    }
-    return await res.json()
-  }).then((data: any) => {
-    const [firstName, lastName] = data.name.split(' ')
-    return { firstName, lastName, email: data.login, authentifiedWithGithub: true }
-  }).catch((err) => {
-    throw err
-  })
-}
-
-export { authentifyWithGoogle, getAccountInfoWithGithubCode }
+export { authentifyWithGoogle }
