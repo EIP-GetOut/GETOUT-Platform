27c9570060a45c8bd481f1ff7f6241e74b732b5c
Erwan Cariou <erwan1.cariou@epitech.eu>
2024-05-26 23:49:25 -0700

    feat(app): :sparkles: adding valid request for the change password feature

--- app/lib/screens/settings/pages/edit_password/edit_password.dart
+++ app/lib/screens/settings/pages/edit_password/edit_password.dart
@@ -13,6 +13,7 @@ import 'package:getout/screens/settings/services/edit_password.dart';
 import 'package:getout/widgets/fields/widgets/title_field.dart';
 import 'package:getout/screens/settings/widget/edit_password_fields.dart';
 import 'package:getout/widgets/show_snack_bar.dart';
+import 'package:getout/constants/http_status.dart';
 import 'package:getout/tools/app_l10n.dart';
 import 'package:getout/tools/status.dart';
 import 'package:getout/tools/tools.dart';
@@ -47,13 +48,13 @@ class EditPasswordPage extends StatelessWidget {
                       padding: EdgeInsets.symmetric(
                           horizontal: 8, vertical: 8),
                       child: OldPasswordField()),
-                  const SizedBox(height: 15),
+                  const SizedBox(height: 30),
                   fieldTitle('Nouveau mot de passe'.toUpperCase(), true),
                   const Padding(
                       padding: EdgeInsets.symmetric(
                           horizontal: 8, vertical: 8),
                       child: NewPasswordField()),
-                  const SizedBox(height: 15),
+                  const SizedBox(height: 30),
                   fieldTitle(appL10n(context)!.confirm_password.toUpperCase(), true),
                   const Padding(
                       padding: EdgeInsets.symmetric(
@@ -100,8 +101,12 @@ class SendNewNewButton extends StatelessWidget {
                 EditPasswordServices()
                     .sendNewPassword(EditPasswordRequestModel(oldPassword: oldPassword, newPassword: newPassword))
                     .then((final EditPasswordResponseModel value) {
-                  if (!value.isSuccessful) {
+                  if (value.statusCode == HttpStatus.UNAUTHORIZED) {
+                    showSnackBar(context, 'Mot de passe actuel incorrect');
+                  } else if (!value.isSuccessful) {
                     showSnackBar(context, 'Une erreur est survenue veuillez réessayer plus tard');
+                  } else {
+                    showSnackBar(context, 'Votre mot de passe a bien été modifié', color: Colors.green);
                   }
                 });
               },
@@ -110,74 +115,3 @@ class SendNewNewButton extends StatelessWidget {
     );
   }
 }
-
-
-/*
-import 'package:flutter/material.dart';
-
-import 'package:getout/constants/http_status.dart';
-import 'package:getout/screens/settings/services/service.dart';
-import 'package:getout/tools/app_l10n.dart';
-import 'package:getout/tools/validator/field.dart';
-import 'package:getout/tools/validator/password.dart';
-import 'package:getout/widgets/fields/password_field.dart';
-import 'package:getout/widgets/button/floating_button.dart';
-
-class EditPasswordPage extends StatelessWidget {
-  const EditPasswordPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final SettingService service = SettingService();
-    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-    String password = '';
-    String newPassword = '';
-    String confirmPassword = '';
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('MOT DE PASSE'),
-        leading: const BackButton(),
-      ),
-      body: SingleChildScrollView(
-          padding: const EdgeInsets.only(
-              left: 8.0, right: 8.0, top: 16.0, bottom: 64.0),
-          child: Form(
-            key: formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-//              mainAxisAlignment: MainAxisAlignment.center, todo remove scroll or set height.
-              children: [
-                PasswordField(
-                    onChanged: (value) => password = value,
-                    validator: (value) => mandatoryValidator(context, password)),
-                NewPasswordField(
-                    onChanged: (value) => newPassword = value,
-                    validator: (value) => newPasswordValidator(context, newPassword)),
-                ConfirmPasswordField(
-                    onChanged: (value) => confirmPassword = value,
-                    validator: (value) => confirmPasswordValidator(context, newPassword, confirmPassword)),
-                const SizedBox(height: 32),
-              ],
-            ),
-          )),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingButton(
-          title: appL10n(context)!.edit_password,
-          onPressed: () async {
-            //todo setup request & error handling
-            if (formKey.currentState!.validate()) {
-              try {
-                StatusResponse response =
-                    await service.changePassword(password, newPassword);
-                if (response.status == HttpStatus.OK) {}
-                //handle response
-              } catch (e) {
-                //handle error
-              }
-            }
-          }),
-    );
-  }
-}
-*/
