1277c6ceecf96937492c6e42074ef6fb13df3d8a
Julien Letoux <julien.letoux@epitech.eu>
2024-06-04 17:57:05 +0200

    feat(backend): :sparkles: patch account

--- backend/src/routes/account.ts
+++ backend/src/routes/account.ts
@@ -6,15 +6,26 @@
 */
 
 import { type Request, type Response, Router } from 'express'
+import { body } from 'express-validator'
 import { StatusCodes, getReasonPhrase } from 'http-status-codes'
 
-import { logApiRequest } from '@services/middlewares/logging'
+import logger, { logApiRequest } from '@services/middlewares/logging'
+import validate from '@services/middlewares/validator'
 import { NotLoggedInError } from '@services/utils/customErrors'
 import { handleErrorOnRoute } from '@services/utils/handleRouteError'
 
-import { deleteAccount } from '@models/account'
+import { deleteAccount, modifyAccount } from '@models/account'
+
+import { type Account } from '@entities/Account'
 
 const router = Router()
+
+const rulesPatch = [
+  body('email').optional().isEmail(),
+  body('firstName').optional().isString(),
+  body('lastName').optional().isString(),
+  body('bornDate').optional().isDate()
+]
 
 router.delete('/account', logApiRequest, (req: Request, res: Response) => {
   if (req.session?.account?.id == null) {
@@ -24,6 +35,18 @@ router.delete('/account', logApiRequest, (req: Request, res: Response) => {
   deleteAccount(req.session).then(() => {
     return res.status(StatusCodes.NO_CONTENT).send(getReasonPhrase(StatusCodes.NO_CONTENT))
   }).catch(handleErrorOnRoute(res))
+})
+
+router.patch('/account', rulesPatch, validate, logApiRequest, (req: Request, res: Response) => {
+  if (req.session?.account?.id == null) {
+    handleErrorOnRoute(res)(new NotLoggedInError())
+    return
+  }
+  modifyAccount(req.session.account.id, req.body as Partial<Account>).then(() => {
+    return res.status(StatusCodes.OK).json(req.body)
+  }).catch((err: any) => {
+    handleErrorOnRoute(err)
+  })
 })
 
 export default router
