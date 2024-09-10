12961196775c2acf58fdcd3fd15fea66cbda811b
Alexandre CHETRIT <chetrit.pro@hotmail.com>
2024-09-10 17:14:26 +0200

    feat(backend): :construction: concurrent get movies details function in movie recommendation algorithm

--- backend/src/services/recommendations/movies/getParameters.py
+++ backend/src/services/recommendations/movies/getParameters.py
@@ -7,12 +7,41 @@
 
 import json
 from collections import Counter
+import asyncio
 
-def getmoviesDetailsConcurrently(parameters):
-    return []
+import os
+from tmdbv3api import TMDb, Movie
+
+
+def fetchMovieDetails(movie_id):
+    tmdb = TMDb()
+    tmdb.api_key = os.getenv("MOVIE_DB_KEY")
+    tmdb.language = 'fr'
+    movieApi = Movie()
+    movieApi.language = 'fr'
+
+    movie = movieApi.details(movie_id)
+    director = next((member for member in movie['casts'].get('crew', []) if member.get('job') == 'Director'), None)
+
+    filteredMovie = {
+        "id": movie.get("id"),
+        "title": movie.get("title"),
+        "genres": [genre['name'] for genre in movie.get("genres", [])],
+        "releaseDate": movie.get("release_date"),
+        "director": director['name']
+    }
+
+    return filteredMovie
+
+
+async def getMoviesDetailsConcurrently(movies: list[int]):
+    tasks = [asyncio.to_thread(fetchMovieDetails, id) for id in movies]
+    movieDetails = await asyncio.gather(*tasks)
+    return movieDetails
+
 
 def getLikedMoviesParameters(parameters: dict) -> dict:
-    data = getmoviesDetailsConcurrently(parameters["likedMovies"])
+    data = asyncio.run(getMoviesDetailsConcurrently(parameters["likedMovies"]))
     genres = []
     if not isinstance(data, list):
         raise ValueError("Expected data to be a list containing another list")
@@ -22,7 +51,7 @@ def getLikedMoviesParameters(parameters: dict) -> dict:
             print(f"Type of item: {type(item)}")
             print(f"Item content: {item}")
             if isinstance(item, dict):
-                genre_ids = item.get('genre_ids', [])
+                genre_ids = item.get('genres', [])
                 if isinstance(genre_ids, dict):
                     genre_ids = list(genre_ids.keys())
                 genres.extend(genre_ids)
@@ -30,7 +59,6 @@ def getLikedMoviesParameters(parameters: dict) -> dict:
                 raise ValueError("Expected each item in nested list to be a dictionary")
     else:
         raise ValueError("Expected data to contain a nested list")
-
     genre_counts = Counter(genres)
     filtered_genre_counts = {
         genre: count for genre, count in genre_counts.items()
@@ -39,13 +67,12 @@ def getLikedMoviesParameters(parameters: dict) -> dict:
     top_genres = sorted(filtered_genre_counts.items(), key=lambda x: x[1], reverse=True)[:3]
     liked_genres = [genre for genre, count in top_genres]
     parameters["likedGenres"] = liked_genres if liked_genres else None
-    # print(parameters)
+    print(parameters)
     return parameters
 
 
-
 def getDislikedMoviesParameters(parameters: json) -> json:
-    data = getmoviesDetailsConcurrently(parameters["likedMovies"])
+    data = asyncio.run(getMoviesDetailsConcurrently(parameters["dislikedMovies"]))
     genres = []
     if not isinstance(data, list):
         raise ValueError("Expected data to be a list containing another list")
@@ -64,8 +91,7 @@ def getDislikedMoviesParameters(parameters: json) -> json:
 
     genre_counts = Counter(genres)
     filtered_genre_counts = {
-        genre: count for genre, count in genre_counts.items()
-        if  count >= 2
+        genre: count for genre, count in genre_counts.items() if count >= 2
     }
     top_genres = sorted(filtered_genre_counts.items(), key=lambda x: x[1], reverse=True)[:3]
     liked_genres = [genre for genre, count in top_genres]
@@ -88,10 +114,10 @@ def getParameters(account: json) -> json:
             "dislikedGenres": None,
             "leastFavouriteEpoch": None
         }
-        if parameters["likedMovies"] is not None:
-            parameters = getLikedMoviesParameters(parameters)
-        if parameters["dislikedMovies"] is not None:
-            parameters = getDislikedMoviesParameters(parameters)
+        # if parameters["likedMovies"] is not None:
+        #     parameters = getLikedMoviesParameters(parameters)
+        # if parameters["dislikedMovies"] is not None:
+            # parameters = getDislikedMoviesParameters(parameters)
         return parameters
     except KeyError as e:
         print(f"Missing key in account data: {e}")
