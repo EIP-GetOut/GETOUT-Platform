fc7f7c7c0c9a255bd46b59c9aa69e4117db968d6
Julien Letoux <julien.letoux@epitech.eu>
2024-09-12 17:07:32 +0200

    feat(backend): :sparkles: add random books if there is not enough books in the pool after all the fetches

--- backend/src/services/recommendations/books/getBooksPool.py
+++ backend/src/services/recommendations/books/getBooksPool.py
@@ -116,45 +116,57 @@ def getBooksByAuthors(bookread: list, authors: list, booksPool: list) -> list:
             booksPoolByAuthors = booksPoolByAuthors[:int(NB_BOOKS * BOOKS_BY_AUTHORS_WEIGHT)]
     return booksPoolByAuthors
 
-def getBooksRandom() -> list:
-    booksPoolRandom = []
-    return booksPoolRandom
+def getBooksRandom(bookread: list, booksPool: list) -> list:
+    booksPoolRandom = booksPool[:]
+    existing_book_ids = {book['id'] for book in booksPool}
+    books_needed = NB_BOOKS - len(booksPool)
+    start_index = 0
+
+    while len(booksPoolRandom) < NB_BOOKS:
+        params = {
+            'maxResults': 40,
+            'startIndex': start_index,
+            'langRestrict': 'fr',
+            'printType': 'books'
+        }
+        try:
+            response = requests.get(API_URL, params=params)
+            response.raise_for_status()
+            books = response.json().get('items', [])
+            if not books:
+                break
+            for book in books:
+                book_info = book.get('volumeInfo', {})
+                book_id = book.get('id', 'Unknown ID')
+                book_title = book_info.get('title', 'Unknown Title')
+                if detect(book_title) == 'fr' and book_title not in bookread and book_id not in existing_book_ids:
+                    book_authors = book_info.get('authors', ['Unknown Author'])
+                    book_genres = book_info.get('categories', ['Unknown Genre'])
+                    published_date = book_info.get('publishedDate', 'Unknown Date')
+                    booksPoolRandom.append({
+                        'id': book_id,
+                        'title': book_title,
+                        'genres': book_genres,
+                        'author': book_authors,
+                        'date': published_date
+                    })
+                    existing_book_ids.add(book_id)
+                    if len(booksPoolRandom) >= NB_BOOKS:
+                        break
+            start_index += 40
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching books: {e}")
+            break
+    random.shuffle(booksPoolRandom)
+    return booksPoolRandom[:books_needed]
+
 
 def getBooksPool(parameters: dict) -> list:
-    # booksPool = []
-    # for genre in parameters['genres']:
-    #     for page in range(int(10 / len(parameters['genres']))):
-    #         url = f'https://www.googleapis.com/books/v1/volumes?q=subject:{urllib.parse.quote_plus(genre)}&printType=books&maxResults=40&startIndex={page * 40}&orderBy=relevance&langRestrict=fr'
-    #         try:
-    #             response = requests.get(url)
-    #             response.raise_for_status()
-    #             data = response.json()
-    #             items = data.get('items', [])
-    #             for item in items:
-    #                 volumeInfo = item.get('volumeInfo', {})
-    #                 if 'imageLinks' not in volumeInfo or 'categories' not in volumeInfo:
-    #                     continue
-    #                 filteredItem = {
-    #                     'id': item.get('id', None),
-    #                     'title': volumeInfo.get('title', None),
-    #                     'authors': volumeInfo.get('authors', None),
-    #                     'publishedDate': volumeInfo.get('publishedDate', None),
-    #                     'pageCount': volumeInfo.get('pageCount', None),
-    #                     'categories': volumeInfo.get('categories', None),
-    #                     'language': volumeInfo.get('language', None)
-    #                 }
-    #                 if not all(filteredItem.values()):
-    #                     continue
-    #                 booksPool.append(filteredItem)
-    #         except requests.exceptions.RequestException as e:
-    #             print(f'HTTP request error: {e}')
-    #         except json.JSONDecodeError as e:
-    #             print(f'Error decoding JSON from response: {e}')
-    #             print(f'Response content: {response.content}')
     booksPool = []
     booksPool.extend(getBooksByGenres(parameters["readBooks"], parameters["genres"], booksPool, BOOKS_BY_GENRES_WEIGHT))
     booksPool.extend(getBooksByGenres(parameters["readBooks"], parameters["likedGenres"], booksPool, BOOKS_BY_GENRES_LIKED_WEIGHT))
     booksPool.extend(getBooksByAuthors(parameters["readBooks"], parameters["favouriteWriters"], booksPool))
-    # if len(booksPool) < NB_BOOKS:
-    #     booksPool.extend(getBooksRandom())
+    if len(booksPool) < NB_BOOKS:
+        booksPool.extend(getBooksRandom(parameters["readBooks"], booksPool))
+    print(len(booksPool))
     return booksPool
