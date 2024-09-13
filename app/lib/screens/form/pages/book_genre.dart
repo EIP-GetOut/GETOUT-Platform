fec69f09f95452e67fea52354fb17de7e39dd8c4
Erwan Cariou <72020065+ErwanC7@users.noreply.github.com>
2024-09-13 02:15:03 +0200

    feat(app): :sparkles: form have a loading button + 3 choices limit (#99)

--- app/lib/screens/form/pages/book_genre.dart
+++ app/lib/screens/form/pages/book_genre.dart
@@ -17,39 +17,38 @@ import 'package:getout/tools/app_l10n.dart';
 import 'package:getout/tools/tools.dart';
 
 class BookGenres extends StatelessWidget {
-  const BookGenres({super.key});
+  final BuildContext formContext;
+
+  const BookGenres({required this.formContext, super.key});
 
   @override
-  Widget build(BuildContext context)
-  {
-    return BlocBuilder<FormBloc, FormStates>(builder: (context, state)
-    {
-      context.read<FormBloc>().add(const EmitEvent(status: FormStatus.bookGenres));
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          SizedBox(height: Tools.heightFactor(context, 0.03)),
-          PageTitle(
-            title: appL10n(context)!.book_genre,
-            description: appL10n(context)!.form_description,
-          ),
-          SizedBox(height: Tools.heightFactor(context, 0.06)),
-          SizedBox(
+  Widget build(BuildContext context) {
+    formContext
+        .read<FormBloc>()
+        .add(const EmitEvent(status: FormStatus.bookGenres));
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: Tools.heightFactor(context, 0.03)),
+        PageTitle(
+          title: appL10n(context)!.book_genre,
+          description: appL10n(context)!.form_description,
+        ),
+        SizedBox(height: Tools.heightFactor(context, 0.06)),
+        SizedBox(
             width: MediaQuery.of(context).size.width,
-              child: const ProgressBar(total: 3, current: 1)),
-          SizedBox(height: Tools.heightFactor(context, 0.05)),
-          Expanded(
-            child: Padding(
+            child: const ProgressBar(total: 3, current: 1)),
+        SizedBox(height: Tools.heightFactor(context, 0.05)),
+        Expanded(
+          child: Padding(
               padding: const EdgeInsets.only(bottom: 65.0),
               child: FormCheckbox(
-                checkboxList: context.read<FormBloc>().state.bookGenres,
+                checkboxList: formContext.read<FormBloc>().state.bookGenres,
                 checkboxImages: null,
                 event: const BookGenresEvent(key: ''),
-              )
-            ),
-          )
-        ],
-      );
-    });
+              )),
+        )
+      ],
+    );
   }
 }
