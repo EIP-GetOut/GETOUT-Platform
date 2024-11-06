eb073f8e2c4903895dc43c66fff8c73ee4e30637
Erwan Cariou <erwan1.cariou@epitech.eu>
2024-11-06 19:33:08 +0100

    feat(app): :lipstick: :recycle: updating settings UI + new  notification system

--- app/lib/screens/settings/pages/notifications/notifications_page.dart
+++ app/lib/screens/settings/pages/notifications/notifications_page.dart
@@ -10,8 +10,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_switch/flutter_switch.dart';
 
+import 'package:getout/widgets/show_snack_bar.dart';
 import 'package:getout/tools/tools.dart';
-// import 'package:getout/tools/app_l10n.dart';
+import 'package:getout/global.dart' as globals;
 
 class NotificationsPage extends StatefulWidget {
   final String value;
@@ -25,38 +26,58 @@ class _NotificationsPageState extends State<NotificationsPage> {
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-          Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-              child: Padding( padding : const EdgeInsets.only(right: 15.0, left: 15.0), child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SvgPicture.asset('assets/images/icon/bell(notification).svg'),
-                    SizedBox(width: Tools.widthFactor(context, 0.065)),
-                    Text(widget.value,
-                        style:
-                            TextStyle(fontSize: 18, color: Colors.grey[800])),
-                    const Expanded(child: SizedBox()),
-                    FlutterSwitch(
-                      height: 35.0,
-                      width: 65.0,
-                      padding: 6.0,
-                      toggleSize: 20.0,
-                      borderRadius: 20.0,
-                      value: false, // globals.notificationsServices.isActive,
-                      activeColor: const Color.fromRGBO(213, 86, 65, 1),
-                      onToggle: (val) {
-                        setState(() {
-                          // globals.notificationsServices.isActive = val;
-                        });
-                        // globals.notificationsServices.isActive = val;
-                        /*if (val == false) {
-                          globals.notificationsServices.stopNotif();
-                        } else {
-                          globals.notificationsServices.sendNotif();
-                        }*/
-                      },
-                    ),
-                  ])))
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            height: Tools.heightFactor(context, 0.06),
+            child: Padding(
+                padding: const EdgeInsets.only(right: 15.0, left: 15.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SvgPicture.asset('assets/images/icon/bell(notification).svg', width: 33),
+                      SizedBox(width: Tools.widthFactor(context, 0.065)),
+                      Text(widget.value,
+                          style:
+                              TextStyle(fontSize:
+                              (MediaQuery.of(context).size.width > 400)
+                                  ? 18
+                                  : 12)),
+                      const Expanded(child: SizedBox()),
+                      FlutterSwitch(
+                        height: 35.0,
+                        width: 65.0,
+                        padding: 6.0,
+                        toggleSize: 20.0,
+                        borderRadius: 20.0,
+                        value: globals.notificationsServices.isNotificationEnable,
+                        activeColor: const Color.fromRGBO(213, 86, 65, 1),
+                        onToggle: (val) {
+                          setState(() {
+                            globals.notificationsServices.isNotificationEnable = val;
+                          });
+                          globals.notificationsServices.isNotificationEnable = val;
+                          globals.notificationsServices.saveEnableNotification();
+                          if (val == true) {
+                            if (globals.notificationsServices.isNotificationPermit == true) {
+                              globals.notificationsServices.scheduleNotification();
+                            } else {
+                              globals.notificationsServices.requestPermission().then((value) {
+                                if (value == null && context.mounted) {
+                                  showSnackBar(context,
+                                      'Vous devez activer les notifications dans les paramètres de votre téléphone',
+                                      color: Colors.red);
+                                }
+                                globals.notificationsServices
+                                    .scheduleNotification();
+                              });
+                            }
+                          } else {
+                            globals.notificationsServices.cancelScheduledNotification(0);
+                          }
+                        },
+                      ),
+                    ])),
+          )
         ]);
   }
 }
