b89cc7a39c25954a25278a7264f3f8326c8827c7
Chouteau Perry <perry.chouteau@epitech.eu>
2024-11-28 17:10:07 +0100

    fix(app): :sparkles: fix errors homepage.

--- app/lib/widgets/object_loading_error_widget.dart
+++ app/lib/widgets/object_loading_error_widget.dart
@@ -26,14 +26,17 @@ import 'package:getout/widgets/transition_page.dart';
         children: [
           const Icon(Boxicons.bx_error, size: 40, color: Colors.red),
           const SizedBox(width: 10), // Add some space between the icon and the text
-          TransitionPage(
-                    title: appL10n(context)!.error_unknown_short,
-                    description: appL10n(context)!.error_unknown_description,
-                    image: 'assets/images/draw/error.svg',
-                    buttonText: appL10n(context)!.error_ok,
-                    nextPage: () => {
-                          Navigator.pop(context),
-                        }),
+          Expanded(
+            child: Text(
+              appL10n(context)!.error_loading(object),
+              textAlign: TextAlign.justify,
+              style: const TextStyle(
+                fontSize: 12,
+                color: Colors.red,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
         ],
       ),
     );
