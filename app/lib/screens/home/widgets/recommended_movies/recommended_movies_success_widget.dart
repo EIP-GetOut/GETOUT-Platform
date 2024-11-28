1a30cc674e25784ac4cf161daf0c1c7bdb7677d3
Chouteau Perry <perry.chouteau@epitech.eu>
2024-11-28 20:27:04 +0100

    fix(app): :sparkles: update carroussel on change.

--- app/lib/screens/home/widgets/recommended_movies/recommended_movies_success_widget.dart
+++ app/lib/screens/home/widgets/recommended_movies/recommended_movies_success_widget.dart
@@ -12,6 +12,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:getout/constants/extern_url.dart';
 
 import 'package:getout/screens/home/bloc/liked_movies/liked_movies_bloc.dart';
+import 'package:getout/screens/home/bloc/recommended_movies/recommended_movies_bloc.dart';
 import 'package:getout/screens/home/bloc/saved_movies/saved_movies_bloc.dart';
 import 'package:getout/screens/home/bloc/watched_movies/watched_movies_bloc.dart';
 import 'package:getout/screens/movie/bloc/movie_provider.dart';
@@ -41,125 +42,127 @@ class RecommendedMoviesSuccessWidget extends StatelessWidget {
             title: appL10n(context)!.movie_recommendations,
             isBooks: false),
         const SizedBox(height: 20),
-        CarouselSlider(
-          options: CarouselOptions(
-            height: 250.0,
-            autoPlay: true,
-            autoPlayInterval: const Duration(seconds: 10),
-            aspectRatio: 1.0,
-            enlargeCenterPage: true,
-            viewportFraction: 0.9,
-          ),
-          items: movies.map((movie) {
-            return Builder(builder: (BuildContext context) {
-              return InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (_) => BlocProvider.value(
-                                value: BlocProvider.of<LikedMoviesHydratedBloc>(
-                                    context),
-                                child: BlocProvider.value(
-                                    value: BlocProvider.of<
-                                        SavedMoviesHydratedBloc>(context),
-                                    child: BlocProvider.value(
-                                        value: BlocProvider.of<
-                                            WatchedMoviesHydratedBloc>(context),
-                                        child: Movie(movie.id))))));
-                  },
-                  child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: const EdgeInsets.symmetric(horizontal: 0),
-                      child: Stack(
-                        children: [
-                          Align(
-                            alignment: Alignment.center,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: NetworkImage(
-                                        '${ExternalConstants.MovieImagePreviewPath}${movie.posterPath!}')),
-                                borderRadius: BorderRadius.circular(20.0),
-                                color: Colors.blue,
+        BlocBuilder<RecommendedMoviesHydratedBloc, RecommendedMoviesState>(
+        builder: (context, state) {
+          return CarouselSlider(
+            options: CarouselOptions(
+              height: 250.0,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 10),
+              aspectRatio: 1.0,
+              enlargeCenterPage: true,
+              viewportFraction: 0.9,
+            ),
+            items: state.recommendedMovies.map((movie) {
+              return Builder(builder: (BuildContext context) {
+                return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => BlocProvider.value(
+                                  value: BlocProvider.of<LikedMoviesHydratedBloc>(
+                                      context),
+                                  child: BlocProvider.value(
+                                      value: BlocProvider.of<
+                                          SavedMoviesHydratedBloc>(context),
+                                      child: BlocProvider.value(
+                                          value: BlocProvider.of<
+                                              WatchedMoviesHydratedBloc>(context),
+                                          child: Movie(movie.id))))));
+                    },
+                    child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.symmetric(horizontal: 0),
+                        child: Stack(
+                          children: [
+                            Align(
+                              alignment: Alignment.center,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage(
+                                          '${ExternalConstants.MovieImagePreviewPath}${movie.posterPath!}')),
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  color: Colors.blue,
+                                ),
                               ),
                             ),
-                          ),
-                          ClipRRect(
-                            borderRadius:
-                                BorderRadius.circular(20), // Clip it cleanly.
+                            ClipRRect(
+                              borderRadius:
+                              BorderRadius.circular(20), // Clip it cleanly.
 
-                            child: Container(
-                              color: Colors.black.withOpacity(0.4),
-                              alignment: Alignment.center,
-                            ),
-                          ),
-                          Align(
                               child: Container(
-                            width: MediaQuery.of(context).size.width,
-                            padding: const EdgeInsets.all(24.0),
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                                color: Colors.black.withOpacity(0.4),
+                                alignment: Alignment.center,
+                              ),
+                            ),
+                            Align(
+                                child: Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  padding: const EdgeInsets.all(24.0),
+                                  child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
 //                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  ///title
-                                  Text(movie.title,
-                                      maxLines: 3,
-                                      style: const TextStyle(
-                                          fontSize: 20,
-                                          color: Colors.white,
-                                          fontFamily: 'Poppins',
-                                          overflow: TextOverflow.ellipsis)),
-                                  const SizedBox(height: 4),
+                                      children: <Widget>[
+                                        ///title
+                                        Text(movie.title,
+                                            maxLines: 3,
+                                            style: const TextStyle(
+                                                fontSize: 20,
+                                                color: Colors.white,
+                                                fontFamily: 'Poppins',
+                                                overflow: TextOverflow.ellipsis)),
+                                        const SizedBox(height: 4),
 
-                                  ///averageRating + releaseDate
-                                  Row(children: [
-                                    if (movie.averageRating != null) ...[
-                                      const Icon(Icons.star_outlined,
-                                          color: Colors.white, size: 20),
-                                      Text(movie.averageRating!.toStringAsFixed(1),
-                                          style: const TextStyle(
-                                            fontSize: 14,
-                                            color: Colors.white,
-                                            fontFamily: 'Poppins',
-                                          )),
-                                    ],
-                                    if (movie.averageRating != null &&
-                                        movie.releaseDate != null) ...[
-                                      const SizedBox(width: 4),
-                                      const Icon(Icons.circle,
-                                          color: Colors.white, size: 5),
-                                      const SizedBox(width: 4),
-                                    ],
-                                    if (movie.releaseDate != null)
-                                      Text(movie.releaseDate!.substring(0, 4),
-                                          style: const TextStyle(
-                                            fontSize: 14,
-                                            color: Colors.white,
-                                            fontFamily: 'Poppins',
-                                          )),
-                                  ]),
+                                        ///averageRating + releaseDate
+                                        Row(children: [
+                                          if (movie.averageRating != null) ...[
+                                            const Icon(Icons.star_outlined,
+                                                color: Colors.white, size: 20),
+                                            Text(movie.averageRating!.toStringAsFixed(1),
+                                                style: const TextStyle(
+                                                  fontSize: 14,
+                                                  color: Colors.white,
+                                                  fontFamily: 'Poppins',
+                                                )),
+                                          ],
+                                          if (movie.averageRating != null &&
+                                              movie.releaseDate != null) ...[
+                                            const SizedBox(width: 4),
+                                            const Icon(Icons.circle,
+                                                color: Colors.white, size: 5),
+                                            const SizedBox(width: 4),
+                                          ],
+                                          if (movie.releaseDate != null)
+                                            Text(movie.releaseDate!.substring(0, 4),
+                                                style: const TextStyle(
+                                                  fontSize: 14,
+                                                  color: Colors.white,
+                                                  fontFamily: 'Poppins',
+                                                )),
+                                        ]),
 
-                                  ///movieGenres
-                                  if (movie.genres != null &&
-                                      movie.genres!.isNotEmpty)
-                                    Padding(
-                                        padding: const EdgeInsets.only(top: 8),
-                                        child: Wrap(
-                                          spacing: 8.0,
-                                          runSpacing: 4.0,
-                                          children: movie.genres!
-                                              .map((tag) => Tag(text: tag))
-                                              .toList(),
-                                        )),
-                                ]),
-                          )),
-                        ],
-                      )));
-            });
-          }).toList(),
-        ),
+                                        ///movieGenres
+                                        if (movie.genres != null &&
+                                            movie.genres!.isNotEmpty)
+                                          Padding(
+                                              padding: const EdgeInsets.only(top: 8),
+                                              child: Wrap(
+                                                spacing: 8.0,
+                                                runSpacing: 4.0,
+                                                children: movie.genres!
+                                                    .map((tag) => Tag(text: tag))
+                                                    .toList(),
+                                              )),
+                                      ]),
+                                )),
+                          ],
+                        )));
+              });
+            }).toList(),
+          );})
       ],
     ));
   }
